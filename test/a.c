61e8350be02b31c766756ea450948ce714c04745
bytecod3 <emwiti658@gmail.com>
2021-07-13 16:28:03 +0300

    initial commit

---
+++ test/a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CHAR_LEN 10
+
+int main(int argc, char *argv[]){
+	char n[CHAR_LEN] = "yur";
+	int len = strlen(n);
+
+	// string length
+	printf("Length: %d/n", len);
+}
